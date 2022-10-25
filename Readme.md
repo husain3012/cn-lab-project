3b6e33a213875449c51dae7219c5fc7b531c9882
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-10-25 13:07:44 +0530

    working nicely

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Sockets in cpp
+
+## Command to compile server and client:
+
+### Server
+
+```g++ -pthread -o server server.cpp```
+
+### Client
+
+```g++ -pthread -o client client.cpp```
--- server.cpp
+++ server.cpp
@@ -10,10 +10,25 @@ void ServerConnectionHandler(Server *server)
     }
 }
 
-
+int max_number = 1000000;
+vector<bool> is_prime(max_number, true);
+void generatePrimes()
+{
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= max_number; i++)
+    {
+        if (is_prime[i] && (long long)i * i <= max_number)
+        {
+            for (int j = i * i; j <= max_number; j += i)
+                is_prime[j] = false;
+        }
+    }
+    cout << "Prime Sieve Generated!\n";
+}
 
 int main()
 {
+    generatePrimes();
     Server server(8080);
     int itr = 0;
     int interval = 1000;
@@ -23,16 +38,37 @@ int main()
         pair<int, string> message = server.Receive();
         if (message.first != -1)
         {
-            cout << message.first << ": " << message.second << endl;
-            vector<ConnectedClient> connected_clients = server.getConnectedClients();
-            // Forward message to all clients except the sender
-            for (int j = 0; j < connected_clients.size(); j++)
+            int received_number;
+            try
             {
-                ConnectedClient connected_client = connected_clients[j];
-                if (connected_client.socket_fd != message.first)
+                received_number = stoi(message.second);
+            }
+            catch (const std::exception &e)
+            {
+                cout << "INVALID NUMBER RECEIVED!";
+                server.Send("SERVER: INVALID NUMBER RECEIVED!", message.first);
+                continue;
+            }
+
+            cout << message.first << ": " << message.second << endl;
+            cout << "Parsed number: " << received_number << endl;
+            vector<ConnectedClient> connected_clients = server.getConnectedClients();
+
+            if (received_number < max_number and is_prime[received_number])
+            {
+                // Forward message to all clients except the sender
+                for (int j = 0; j < connected_clients.size(); j++)
                 {
-                    server.Send(to_string(message.first) + ": " + message.second, connected_client.socket_fd);
+                    ConnectedClient connected_client = connected_clients[j];
+                    if (connected_client.socket_fd != message.first)
+                    {
+                        server.Send(to_string(message.first) + ": " + message.second, connected_client.socket_fd);
+                    }
                 }
+            }
+            else
+            {
+                server.Send("SERVER: Non Prime number received!", message.first);
             }
         }
     }
